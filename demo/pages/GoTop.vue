1df60f23df2c5e70fc0ed42b184b373688fb9a90
Owlism <897913904@qq.com>
2020-04-17 17:14:33 +0800

    增加了案例

--- demo/pages/GoTop.vue
+++ demo/pages/GoTop.vue
@@ -3,7 +3,10 @@
     <div class="our-h-screen our-w-full"></div>
     <div class="our-h-screen our-w-full"></div>
     <div class="our-h-screen our-w-full"></div>
-    <OurGoTop />
+    <our-go-top>
+      <div class="our-border our-border-black our-p-2">TOP</div>
+    </our-go-top>
+    <our-go-top :right="100" />
   </div>
 </template>
 
